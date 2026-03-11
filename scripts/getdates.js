2ebee85534ef5ce4a4324616656c5d34a0c9535d
wycliff byakagaba <spearkickboxing20@gmail.com>
2026-03-12 01:05:41 +0300

    temples.html styles/temples.css styles/temples-large.css scripts/getdates.js temples.html

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,10 +1,19 @@
+// Footer dates
 const year = document.querySelector("#currentyear");
 const modified = document.querySelector("#lastModified");
 
 if (year) {
-year.textContent = new Date().getFullYear();
+  year.textContent = new Date().getFullYear();
 }
 
 if (modified) {
-modified.textContent = "Last Modified: " + document.lastModified;
+  modified.textContent = "Last Modified: " + document.lastModified;
 }
+
+// Hamburger menu
+const menu = document.querySelector("#menu");
+const nav = document.querySelector(".navigation");
+
+menu.addEventListener("click", () => {
+  nav.classList.toggle("open");
+});
