155bc44a3f9b7e4a703fdeccc4c43e7d230fe3a9
wycliff byakagaba <spearkickboxing20@gmail.com>
2026-03-11 07:30:50 +0300

    temples.html images/accra-ghana.jpg images/london-england.jpg images/nairobi-kenya.jpg images/nauvoo-illinois.jpg images/paris-france.jpg images/rome-italy.jpg images/salt-lake.jpg images/sydney-australia.jpg images/tokyo-japan.jpg scripts/temples.js styles/temples-large.css styles/temples.css

---
+++ scripts/temples.js
@@ -0,0 +1,10 @@
+const menuButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+menuButton.addEventListener("click", () => {
+navigation.classList.toggle("open");
+menuButton.classList.toggle("open");
+});
+
+document.querySelector("#year").textContent = new Date().getFullYear();
+document.querySelector("#lastModified").textContent = document.lastModified;
