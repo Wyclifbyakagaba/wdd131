20586142442c210464ca4864fd24ae670259254a
wycliff byakagaba <spearkickboxing20@gmail.com>
2026-03-20 11:58:08 +0300

    place.html scripts/place.js styles/place.css

--- scripts/place.js
+++ scripts/place.js
@@ -1,21 +1,17 @@
-// FOOTER DATES
+// Footer
 document.getElementById("year").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = document.lastModified;
 
-// STATIC VALUES
-const temp = 45;
-const wind = 5;
-
-// ONE-LINE FUNCTION
-function calculateWindChill(t, w) {
-  return 35.74 + 0.6215*t - 35.75*Math.pow(w,0.16) + 0.4275*t*Math.pow(w,0.16);
+// Wind Chill Calculation
+function calculateWindChill(temp, wind) {
+  return 35.74 + 0.6215 * temp - 35.75 * Math.pow(wind, 0.16) + 0.4275 * temp * Math.pow(wind, 0.16);
 }
 
-// DISPLAY RESULT
-const output = document.getElementById("windchill");
+const temp = parseFloat(document.getElementById("temp").textContent);
+const wind = parseFloat(document.getElementById("wind").textContent);
 
 if (temp <= 50 && wind > 3) {
-  output.textContent = calculateWindChill(temp, wind).toFixed(1) + " °F";
+  document.getElementById("windchill").textContent = calculateWindChill(temp, wind).toFixed(1) + "°F";
 } else {
-  output.textContent = "N/A";
+  document.getElementById("windchill").textContent = "N/A";
 }
